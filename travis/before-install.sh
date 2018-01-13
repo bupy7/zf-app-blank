2897c983228049cb071aecb33a7b473369c5e191
Vasily Belosludcev <bupy765@gmail.com>
2018-01-14 00:22:50 +0500

    fix travis.yml

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -53,7 +53,10 @@ npm install -g uglify-js
 
 # ruby and gem
 # ------------
-apt-get -y install ruby-full
+apt-get -y install software-properties-common
+apt-add-repository -y ppa:brightbox/ruby-ng
+apt-get update
+apt-get -y install ruby2.4
 
 # sass
 # ----
